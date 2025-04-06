1d1530cb031daf920ff78a5bb686e04e871c0983
Yak <107325508+TheTrueYak@users.noreply.github.com>
2025-04-06 03:18:59 -0500

    1.21.1 port!!
    fuck 1.19.2 🙏

--- src/main/java/net/yak/glintless/Glintless.java
+++ src/main/java/net/yak/glintless/Glintless.java
@@ -3,17 +3,16 @@ package net.yak.glintless;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.item.Item;
-import net.minecraft.tag.TagKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Glintless implements ModInitializer {
 	public static final String MOD_ID = "glintless";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final TagKey<Item> NO_GLINT = TagKey.of(Registry.ITEM_KEY, Identifier.of(MOD_ID, "no_glint"));
-
+	public static final TagKey<Item> NO_GLINT = TagKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, "no_glint"));
 	@Override
 	public void onInitialize() {
 
--- src/main/java/net/yak/glintless/mixin/ItemStackMixin.java
+++ src/main/java/net/yak/glintless/mixin/ItemStackMixin.java
@@ -1,18 +1,22 @@
 package net.yak.glintless.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.minecraft.item.ExperienceBottleItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.TagKey;
 import net.yak.glintless.Glintless;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(ExperienceBottleItem.class)
-public abstract class ExperienceBottleItemMixin {
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin {
+
+    @Shadow public abstract boolean isIn(TagKey<Item> tag);
 
     @ModifyReturnValue(method = "hasGlint", at = @At("RETURN"))
-    private boolean glintless$removeBottleOfEnchantingGlint(boolean original, ItemStack stack) {
-        if (stack.isIn(Glintless.NO_GLINT)) {
+    private boolean glintless$removeItemStackGlint(boolean original) {
+        if (this.isIn(Glintless.NO_GLINT)) {
             return false;
         }
         return original;
